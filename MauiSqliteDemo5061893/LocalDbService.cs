a59d5c04be983b281ed9de6fbc2e57c79cb82fe9
Julissa <Julissa@JULS>
2024-08-19 13:43:32 -0600

    Agregar archivos de proyecto.

---
+++ MauiSqliteDemo5061893/LocalDbService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiSqliteDemo5061893
+{
+    public  class LocalDbService
+    {
+        private const string DB_NAME = "demo_local_db.db3";
+        private readonly SQLiteAsyncConnection _connection;
+        
+        public LocalDbService()
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+
+            //Le indica al sistema que crea la tabla de nuestro contexto
+            _connection.CreateTableAsync<Clientes>();
+        }
+        public async Task<List<Clientes>> GetClientes()
+        {
+            return await _connection.Table<Clientes>().ToListAsync();
+        }
+        //metodo para alistar los registro por id
+        public async Task<Clientes> GetById(int id)
+        {
+            return await _connection.Table<Clientes>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        //metodo para crear registro
+        public async Task Create(Clientes clientes)
+        {
+            await _connection.InsertAsync(clientes);
+        }
+
+        //Metodo para actualizar
+        public async Task Update(Clientes clientes)
+        {
+            await _connection.UpdateAsync(clientes);
+        }
+
+        //Metodo para eliminar
+        public async Task Delete(Clientes clientes)
+        {
+            await _connection.DeleteAsync(clientes);
+        }
+    }
+}
---
+++ MauiSqliteDemo5061893/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿namespace MauiSqliteDemo5061893
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly LocalDbService _dbService;
+        private int _editClientesId;
+        
+
+        public MainPage(LocalDbService dbService)
+        {
+            InitializeComponent();
+            _dbService = dbService;
+            Task.Run(async () => Listview.ItemsSource = await _dbService.GetClientes());
+
+        }
+
+       
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            if (_editClientesId == 0)
+            {
+                //agrega cliente
+                await _dbService.Create(new Clientes
+                {
+                    NombreCliente = nombreEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Movil = movilEntryField.Text
+                });
+            }
+
+            else
+            {
+                //Edita cliente
+                await _dbService.Update(new Clientes
+                {
+                    Id = _editClientesId,
+                    NombreCliente = nombreEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Movil = movilEntryField.Text
+
+                });
+                _editClientesId = 0;
+            }
+            nombreEntryField.Text = string.Empty;
+            emailEntryField.Text = string.Empty;
+            movilEntryField.Text = string.Empty;
+
+            Listview.ItemsSource = await _dbService.GetClientes();
+        }
+    
+    
+
+        private async void Listview_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var clientes = (Clientes)e.Item;
+            var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
+
+            switch (action)
+            {
+                case "Edit":
+                    _editClientesId = clientes.Id;
+                    nombreEntryField.Text = clientes.NombreCliente;
+                    emailEntryField.Text = clientes.Email;
+                    movilEntryField.Text = clientes.Movil;
+                    break;
+
+                case "Delete":
+                    await _dbService.Delete(clientes);
+                    Listview.ItemsSource = await _dbService.GetClientes();
+                    break;
+            }
+        }
+    }
+
+}
